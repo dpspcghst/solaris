21014b8ce1ae1b321c89e8b406b40b3c11b9b641
Hemingway <ghostblac@protonmail.com>
2022-01-22 21:38:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,39 @@ wall_surface2.fill(dark_lime_green1)
 clock = game.time.Clock()
 
 
+def collision_detection(rect1, rect2, rect3):
+    """
+    """
+
+    if rect1.colliderect(rect2):
+
+        if player_rectangle.x > 0:  # Moving right; Hit left side of wall
+            player_rectangle.right = wall_rectangle1.left
+
+        if player_rectangle.x < 0:  # Moving left; Hit right side of wall
+            player_rectangle.left = wall_rectangle1.right
+
+        if player_rectangle.y > 0:  # Moving down; Hit top side of wall
+            player_rectangle.bottom = wall_rectangle1.top
+
+        if player_rectangle.y < 0:  # Moving up; Hit bottom side of wall
+            player_rectangle.top = wall_rectangle1.bottom
+
+    if rect1.colliderect(rect3):
+
+        if player_rectangle.x > 0:  # Moving right; Hit left side of wall
+            player_rectangle.right = wall_rectangle2.left
+
+        if player_rectangle.x < 0:  # Moving left; Hit right side of wall
+            player_rectangle.left = wall_rectangle2.right
+
+        if player_rectangle.y > 0:  # Moving down; Hit top side of wall
+            player_rectangle.bottom = wall_rectangle2.top
+
+        if player_rectangle.y < 0:  # Moving up; Hit bottom side of wall
+            player_rectangle.top = wall_rectangle2.bottom
+
+
 def event_loop():
     """
     """
@@ -44,6 +77,17 @@ def event_loop():
         if event.type == game.QUIT:
             game.quit()
             exit()
+
+
+def go(x, y):
+    """
+    """
+
+    if x != 0:
+        single_axis_go(x, 0)
+
+    if y != 0:
+        single_axis_go(0, y)
 
 
 def graphics_renderer(image1, image2, image3):
@@ -56,7 +100,7 @@ def graphics_renderer(image1, image2, image3):
     screen.blit(wall_surface2, (image3.x, image3.y))
 
 
-def player_input(person):
+def player_input(rectangle):
     """
     """
 
@@ -65,50 +109,34 @@ def player_input(person):
     if keys_pressed[game.K_DOWN]:
 
         if keys_pressed[game.K_LSHIFT]:
-            person.y += running_speed
+            go(0, running_speed)
 
         else:
-            person.y += walking_speed
+            go(0, walking_speed)
 
     if keys_pressed[game.K_LEFT]:
 
         if keys_pressed[game.K_LSHIFT]:
-            person.x -= running_speed
+            go(-running_speed, 0)
 
         else:
-            person.x -= walking_speed
+            go(-walking_speed, 0)
 
     if keys_pressed[game.K_RIGHT]:
 
         if keys_pressed[game.K_LSHIFT]:
-            person.x += running_speed
+            go(running_speed, 0)
 
         else:
-            person.x += walking_speed
+            go(walking_speed, 0)
 
     if keys_pressed[game.K_UP]:
 
         if keys_pressed[game.K_LSHIFT]:
-            person.y -= running_speed
+            go(0, -running_speed)
 
         else:
-            person.y -= walking_speed
-
-
-def test_function(rect1, rect2, rect3):
-    """
-    """
-
-    if rect1.colliderect(rect2):
-        print("Wall!")
-        player_rectangle.left = wall_rectangle1.right
-
-    elif rect1.colliderect(rect3):
-        print("Wall!")
-        player_rectangle.right = wall_rectangle2.left
-
-    else:
-        print("No Wall!")
+            go(0, -walking_speed)
 
 
 def main_loop():
@@ -119,9 +147,45 @@ def main_loop():
         event_loop()
         player_input(player_rectangle)
         graphics_renderer(player_rectangle, wall_rectangle1, wall_rectangle2)
-        test_function(player_rectangle, wall_rectangle1, wall_rectangle2)
+        # collision_detection(player_rectangle, wall_rectangle1, wall_rectangle2)
         game.display.update()
         clock.tick(60)
+
+
+def single_axis_go(x, y):
+    """
+    """
+
+    player_rectangle.x += x
+    player_rectangle.y += y
+
+    if player_rectangle.colliderect(wall_rectangle1):
+
+        if x > 0:  # Moving right; Hit left side of wall
+            player_rectangle.right = wall_rectangle1.left
+
+        if x < 0:  # Moving left; Hit right side of wall
+            player_rectangle.left = wall_rectangle1.right
+
+        if y > 0:  # Moving down; Hit top side of wall
+            player_rectangle.bottom = wall_rectangle1.top
+
+        if y < 0:  # Moving up; Hit bottom side of wall
+            player_rectangle.top = wall_rectangle1.bottom
+
+    if player_rectangle.colliderect(wall_rectangle2):
+
+        if x > 0:  # Moving right; Hit left side of wall
+            player_rectangle.right = wall_rectangle2.left
+
+        if x < 0:  # Moving left; Hit right side of wall
+            player_rectangle.left = wall_rectangle2.right
+
+        if y > 0:  # Moving down; Hit top side of wall
+            player_rectangle.bottom = wall_rectangle2.top
+
+        if y < 0:  # Moving up; Hit bottom side of wall
+            player_rectangle.top = wall_rectangle2.bottom
 
 
 if __name__ == "__main__":
