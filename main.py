47c8061fb79087c116637c3d45f83f2b2c8cbc76
Hemingway <ghostblac@protonmail.com>
2022-01-25 13:27:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
+# Research Control building structure for level design
+
 from sys import exit
 
 import pygame as game
 
 game.init()
 
-dark_lime_green1 = (0, 137, 0)  # dark lime green, first shade
-dark_lime_green2 = (0, 177, 0)  # dark lime green, second shade
-height = 450
+BLACK = (0, 0, 0)
+CAPTION = "Solaris"
+DARK_LIME_GREEN1 = (0, 137, 0)  # dark lime green, first shade
+DARK_LIME_GREEN2 = (0, 177, 0)  # dark lime green, second shade
+MODE_HEIGHT = 450
+MODE_WIDTH = 850
 player_height = 25
 player_width = 13
-pure_lime_green = (0, 235, 0)  # pure lime green
-running_speed = 2.85
-very_dark_lime_green1 = (0, 39, 0)  # very dark lime green, first shade
-very_dark_lime_green2 = (0, 98, 0)  # very dark lime green, second shade
-walking_speed = 1.25
+PURE_LIME_GREEN = (0, 235, 0)  # pure lime green
+VERY_DARK_LIME_GREEN1 = (0, 39, 0)  # very dark lime green, first shade
+VERY_DARK_LIME_GREEN2 = (0, 98, 0)  # very dark lime green, second shade
 wall_height = 125
 wall_width = 13
-width = 850
 
-screen = game.display.set_mode((width, height))
-game.display.set_caption("Solaris")
+# End initial variable assignments.
+
+screen = game.display.set_mode((MODE_WIDTH, MODE_HEIGHT))
+game.display.set_caption(CAPTION)
 
 player_rectangle = game.Rect(53, 53, player_width, player_height)
 wall_rectangle1 = game.Rect(125, 125, wall_width, wall_height)
 wall_rectangle2 = game.Rect(250, 125, wall_width, wall_height)
 
-player_surface = game.Surface((player_width, player_height))
-player_surface.fill(very_dark_lime_green1)
-wall_surface1 = game.Surface((wall_width, wall_height))
-wall_surface1.fill(dark_lime_green1)
-wall_surface2 = game.Surface((wall_width, wall_height))
-wall_surface2.fill(dark_lime_green1)
+exit_surface = game.Surface((player_width, player_height))
+exit_surface.fill(BLACK)
 
-clock = game.time.Clock()
+
+def change_game_mode():
+    """
+    """
+
+    pass
 
 
 def event_loop():
@@ -61,63 +66,68 @@ def graphics_renderer():
     """
     """
 
-    screen.fill(pure_lime_green)
+    player_surface = game.Surface((player_width, player_height))
+    player_surface.fill(VERY_DARK_LIME_GREEN1)
+    wall_surface1 = game.Surface((wall_width, wall_height))
+    wall_surface1.fill(DARK_LIME_GREEN1)
+    wall_surface2 = game.Surface((wall_width, wall_height))
+    wall_surface2.fill(DARK_LIME_GREEN1)
+
+    screen.fill(PURE_LIME_GREEN)
     screen.blit(player_surface, (player_rectangle.x, player_rectangle.y))
     screen.blit(wall_surface1, (wall_rectangle1.x, wall_rectangle1.y))
     screen.blit(wall_surface2, (wall_rectangle2.x, wall_rectangle2.y))
+    game.display.update()
 
 
-def player_input():
+def level_algorithm():
     """
     """
 
-    keys_pressed = game.key.get_pressed()
-
-    if keys_pressed[game.K_DOWN]:
-
-        if keys_pressed[game.K_LSHIFT]:
-            go(0, running_speed)
-
-        else:
-            go(0, walking_speed)
-
-    if keys_pressed[game.K_LEFT]:
-
-        if keys_pressed[game.K_LSHIFT]:
-            go(-running_speed, 0)
-
-        else:
-            go(-walking_speed, 0)
-
-    if keys_pressed[game.K_RIGHT]:
-
-        if keys_pressed[game.K_LSHIFT]:
-            go(running_speed, 0)
-
-        else:
-            go(walking_speed, 0)
-
-    if keys_pressed[game.K_UP]:
-
-        if keys_pressed[game.K_LSHIFT]:
-            go(0, -running_speed)
-
-        else:
-            go(0, -walking_speed)
+    pass
 
 
 def main_loop():
     """
     """
 
+    clock = game.time.Clock()
+
     while True:
         # Process inputs
         player_input()
         event_loop()
+        # Update game world
         # Generate outputs
         graphics_renderer()
-        game.display.update()
         clock.tick(60)
+
+
+def music_loop():
+    """
+    """
+
+    pass
+
+
+def player_input():
+    """
+    """
+
+    MOVING_SPEED = 2.05
+    keys_pressed = game.key.get_pressed()
+
+    if keys_pressed[game.K_DOWN]:
+        go(0, MOVING_SPEED)
+
+    if keys_pressed[game.K_LEFT]:
+        go(-MOVING_SPEED, 0)
+
+    if keys_pressed[game.K_RIGHT]:
+        go(MOVING_SPEED, 0)
+
+    if keys_pressed[game.K_UP]:
+        go(0, -MOVING_SPEED)
 
 
 def single_axis_go(x, y):
@@ -128,32 +138,51 @@ def single_axis_go(x, y):
     player_rectangle.y += y
 
     if player_rectangle.colliderect(wall_rectangle1):
-
-        if x > 0:  # Moving right; Hit left side of wall
-            player_rectangle.right = wall_rectangle1.left
-
-        if x < 0:  # Moving left; Hit right side of wall
-            player_rectangle.left = wall_rectangle1.right
-
-        if y > 0:  # Moving down; Hit top side of wall
-            player_rectangle.bottom = wall_rectangle1.top
-
-        if y < 0:  # Moving up; Hit bottom side of wall
-            player_rectangle.top = wall_rectangle1.bottom
+        wall1_collisions(x, y)
 
     if player_rectangle.colliderect(wall_rectangle2):
+        wall2_collisions(x, y)
 
-        if x > 0:  # Moving right; Hit left side of wall
-            player_rectangle.right = wall_rectangle2.left
 
-        if x < 0:  # Moving left; Hit right side of wall
-            player_rectangle.left = wall_rectangle2.right
+def title_screen():
+    """
+    """
 
-        if y > 0:  # Moving down; Hit top side of wall
-            player_rectangle.bottom = wall_rectangle2.top
+    pass
 
-        if y < 0:  # Moving up; Hit bottom side of wall
-            player_rectangle.top = wall_rectangle2.bottom
+
+def wall1_collisions(x, y):
+    """
+    """
+
+    if x > 0:  # Going right; Hit left side of wall
+        player_rectangle.right = wall_rectangle1.left
+
+    if x < 0:  # Going left; Hit right side of wall
+        player_rectangle.left = wall_rectangle1.right
+
+    if y > 0:  # Moving down; Hit top side of wall
+        player_rectangle.bottom = wall_rectangle1.top
+
+    if y < 0:  # Moving up; Hit bottom side of wall
+        player_rectangle.top = wall_rectangle1.bottom
+
+
+def wall2_collisions(x, y):
+    """
+    """
+
+    if x > 0:  # Going right; Hit left side of wall
+        player_rectangle.right = wall_rectangle2.left
+
+    if x < 0:  # Going left; Hit right side of wall
+        player_rectangle.left = wall_rectangle2.right
+
+    if y > 0:  # Going down; Hit top side of wall
+        player_rectangle.bottom = wall_rectangle2.top
+
+    if y < 0:  # Going up; Hit bottom side of wall
+        player_rectangle.top = wall_rectangle2.bottom
 
 
 if __name__ == "__main__":
