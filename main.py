e64e3946cc4742301a31c93a59047fc36f94bb09
Hemingway <ghostblac@protonmail.com>
2022-01-31 04:28:49 -0500

    Corrected grammar.

--- main.py
+++ main.py
@@ -425,7 +425,7 @@ def event_loop():
 
 def get_walls():
     """
-    Lengthen walls 13 and 14 until the meet. Move walls 9 and 10 by pixels. Lengthen wall 5 by 16 pixels.
+    Lengthen walls 13 and 14 until they meet. Move walls 9 and 10 by 16 pixels. Lengthen wall 5 by 16 pixels.
     """
 
     walls = []
