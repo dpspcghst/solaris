42d6cf9a13952dcfecd034df1e4fa288a0cf3169
Hemingway <ghostblac@protonmail.com>
2022-02-03 14:05:54 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,7 +1,34 @@
-import pygae as game
+import pygame as game
+
+from settings import *
 
 
-class Player():
+class Player(game.sprite.Sprite):
+    """
+    """
+
+    def __init__(self, groups, pos):
+        """
+        """
+
+        super().__init__(groups)
+        self.image = game.Surface((TILE_WIDTH, TILE_HEIGHT))
+        self.rect = self.image.get_rect(topleft=pos)
+
+    def get_image(self):
+        """
+        """
+
+        return self.image
+
+    def get_rect(self):
+        """
+        """
+
+        return self.rect
+
+
+class Old():
     """
     """
 
