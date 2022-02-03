b417d818e725cdc0ca3b68735c91d70f96a543ed
Hemingway <ghostblac@protonmail.com>
2022-02-03 14:06:56 -0500

    Create tile.py

---
+++ tile.py
@@ -0,0 +1,28 @@
+import pygame as game
+
+from settings import *
+
+
+class Tile(game.sprite.Sprite):
+    """
+    """
+
+    def __init__(self, groups, pos):
+        """
+        """
+
+        super().__init__(groups)
+        self.image = game.Surface((TILE_WIDTH, TILE_HEIGHT))
+        self.rect = self.image.get_rect(topleft=pos)
+
+    def get_image(self):
+        """
+        """
+
+        return self.image
+
+    def get_rect(self):
+        """
+        """
+
+        return self.rect
